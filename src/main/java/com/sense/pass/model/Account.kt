6cbdeb5a01d3da956df5969aebfb43d5cf30c674
playsenseme <playsenseme@gmail.com>
2022-01-03 01:32:57 +0300

    Add Account Model

--- src/main/java/com/sense/pass/model/Account.kt
+++ src/main/java/com/sense/pass/model/Account.kt
@@ -1,16 +1,29 @@
 package com.sense.pass.model
 
 import org.hibernate.annotations.GenericGenerator
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import javax.persistence.*
 
-@Entity //Defines that a class can be mapped to a table
+@Entity // Defines that a class can be mapped to a table
 data class Account(
 
-        @Id //Specifies the primary key of the entity
+        // Customer:1 ---> n:Account:1 ---> n:Transaction
+
+        @Id // Specifies the primary key of the entity
         @GeneratedValue(generator = "UUID") //Specifies the primary key generation strategy, which is "UUID" here
-        @GenericGenerator(name = "UUID", strategy = org.hibernate.id.UUIDGenerator)
+        @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String?, // "val" is immutable
+        val balance: BigDecimal? = BigDecimal.ZERO,
+        val creationDate: LocalDateTime,
+
+        @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+        @JoinColumn(name = "customer_id", nullable = false)
+        val customer: Customer?,
+
+        @OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+        val transaction: Set<Transection>?
+
 
 
 )
